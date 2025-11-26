ad2312532315bf6051090d06a524b2ffde8a6964
ForForm <124023903+ForForm@users.noreply.github.com>
2025-11-26 06:20:54 +0300

    Add project files.

---
+++ WebApplication2/DenemeService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WebApplication2
+{
+    public class DenemeService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested) {
+                Console.WriteLine("sdfsdfdsf");
+                await Task.Delay(1000);   
+            }
+            
+        }
+    }
+}
---
+++ WebApplication2/IDeneme.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication2
+{
+    public interface IDeneme
+    {
+        public bool sendMail(string content);
+    }
+
+
+    class Deneme : IDeneme
+    {
+        public bool sendMail(string content)
+        {
+            Console.WriteLine(content);
+            return true;
+        }
+    }
+}
